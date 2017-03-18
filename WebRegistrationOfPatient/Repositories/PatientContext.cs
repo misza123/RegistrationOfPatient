dc24821127ffa96d7a71c2174a6da6266fec2f33
Michal Mielczarek <mmielczarek12@gmail.com>
2017-03-18 14:19:18 +0100

    add PatientContext

---
+++ WebRegistrationOfPatient/Repositories/PatientContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebRegistrationOfPatient.Models;
+
+namespace WebRegistrationOfPatient.Repositories
+{
+    public class PatientContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(Configuration.DefaultConnection);
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Address> Address { get; set; }
+    }
+}
