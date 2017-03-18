4e6c9a55acc52426bcb7930b345939529cf023d6
Michal Mielczarek <mmielczarek12@gmail.com>
2017-03-18 17:52:12 +0100

    add index.html as defaultPage

---
+++ WebRegistrationOfPatient/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebRegistrationOfPatient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Redirect(Url.Content("~/app/index.html"));
+        }
+    }
+}
--- WebRegistrationOfPatient/Startup.cs
+++ WebRegistrationOfPatient/Startup.cs
@@ -62,6 +62,9 @@ namespace WebRegistrationOfPatient
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "homePage", template: "{*url}", defaults: new { controller = "Home", action = "Index" });
             });
         }
     }
