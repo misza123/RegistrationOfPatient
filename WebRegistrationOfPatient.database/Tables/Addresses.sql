d14cd8b274fba462f4a152293bbfcd094496af07
Michal Mielczarek <mmielczarek12@gmail.com>
2017-03-19 23:23:49 +0100

    Dodanie tabeli patient i address

---
+++ WebRegistrationOfPatient.database/Tables/Addresses.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Addresses]
+    (
+      [Id] INT IDENTITY
+               NOT NULL ,
+      [City] VARCHAR(128) NOT NULL ,
+      [Street] VARCHAR(128) ,
+      [HouseNumber] VARCHAR(8) ,
+      [FlatNumber] VARCHAR(8) ,
+      [PostalCode] VARCHAR(6)
+        CONSTRAINT [PK_Id_Addresses] PRIMARY KEY ( [Id] ),
+	 
+    );
---
+++ WebRegistrationOfPatient.database/Tables/Patients.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Patients](
+	[Id] [INT] IDENTITY NOT NULL,
+	[AddressId] [INT] NULL,
+	[name] [VARCHAR](128) NULL,
+	[surname] [VARCHAR](128) NULL,
+	[personalIdentityNumber] [VARCHAR](128) NOT null, 
+    CONSTRAINT [PK_Id_dboPatients] PRIMARY KEY ([Id]),
+	CONSTRAINT [U_personalIdentityNumber_dboPatients] UNIQUE ([personalIdentityNumber]),
+	CONSTRAINT [FK_AddressId_dboPatients] FOREIGN KEY ([AddressId]) REFERENCES [dbo].[Addresses]
+)
