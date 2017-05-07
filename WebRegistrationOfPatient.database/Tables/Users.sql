532a7ab18c35c8e442aaa346a9d6591462f15c6f
Michal Mielczarek <mmielczarek12@gmail.com>
2017-05-08 01:01:29 +0200

    Unique UserLogin and drop UserType from db

--- WebRegistrationOfPatient.database/Tables/Users.sql
+++ WebRegistrationOfPatient.database/Tables/Users.sql
@@ -5,10 +5,10 @@
 	[EmployeeId] INT NULL,
 	[Login] VARCHAR(255) NOT NULL,
 	[Password] VARCHAR(255) NOT NULL,
-	[UserTypeId] INT NOT NULL,
 	
 	CONSTRAINT [PK_Id_Users] PRIMARY KEY ([Id]),
 	--CONSTRAINT [FK_UserTypeId_Users] FOREIGN KEY ([UserTypeId]) REFERENCES [dbo].[UserTypes],
 	CONSTRAINT [FK_PatientId_Users] FOREIGN KEY ([PatientId]) REFERENCES [dbo].[Patients]  ON DELETE SET NULL,
 	CONSTRAINT [FK_EmployeeId_Users]  FOREIGN KEY ([EmployeeId]) REFERENCES [dbo].[Employees] ON DELETE SET NULL,
+	CONSTRAINT [U_Login_Users] UNIQUE ([Login])
 )
