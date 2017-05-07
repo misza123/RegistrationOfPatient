a60c533e1cf0194d82633b3294701b28f483f267
Michal Mielczarek <mmielczarek12@gmail.com>
2017-05-08 00:59:59 +0200

    Use selfHosting - kestrel

--- WebRegistrationOfPatient/Program.cs
+++ WebRegistrationOfPatient/Program.cs
@@ -10,7 +10,7 @@ namespace WebRegistrationOfPatient
       var host = new WebHostBuilder()
         .UseKestrel()
         .UseContentRoot(Directory.GetCurrentDirectory())
-        .UseIISIntegration()
+        .UseKestrel()
         .UseStartup<Startup>()
         .UseApplicationInsights()
         .Build();
